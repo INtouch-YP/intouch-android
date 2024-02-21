6c1a849b824dcecab41344988545eab8db846224
Ilnur Valiev <102486283+JudjinGM@users.noreply.github.com>
2024-02-21 18:34:00 +0400

    Updated Gradle to 8.6, AGP to 8.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,14 +51,19 @@ android {
 
 dependencies {
 
+    //Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+    //Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
