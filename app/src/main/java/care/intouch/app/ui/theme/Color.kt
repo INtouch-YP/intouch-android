5cbf8d8afd470bebdb38f8686095cc4304b8c9df
ImNia <agnia-kimi1996@mail.ru>
2024-02-23 18:03:25 +0700

    Added module uikit

--- app/src/main/java/care/intouch/app/ui/theme/Color.kt
+++ app/src/main/java/care/intouch/app/ui/theme/Color.kt
@@ -9,25 +9,3 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
-val mainColorBlue = Color(0xFFE1F4F8)
-val mainColorGreen = Color(0xFF417D88)
-val mainColorGreen40 = Color(0x66417D88)
-
-val accentColorYellow = Color(0xFFFFF5DE)
-val accentColorGreen = Color(0xFF338C8B)
-val accentColorGreen30 = Color(0x4D338C8B)
-val accentColorGreen50 = Color(0x80338C8B)
-
-val textColorBlue = Color(0xFF1F304F)
-val textColorBlue50 = Color(0x801F304F)
-
-val textColorGreen = Color(0xFF0B4A56)
-val textColorGreen40 = Color(0x66417D88)
-
-val unableElementsColorLight = Color(0xFFEBF4F1)
-val unableElementsColorDark = Color(0xFFD9D9D9)
-
-val inputColor = Color(0xFFFFFFFF)
-val inputColor40 = Color(0x66FFFFFF)
-val inputColor85 = Color(0xD9FFFFFF)
--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -1,23 +1,17 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
-    namespace = "care.intouch.app"
+    namespace = "care.intouch.uikil"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "care.intouch.app"
         minSdk = 23
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,34 +30,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
-
-    implementation(project(":uikit"))
+    implementation(libs.material)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
