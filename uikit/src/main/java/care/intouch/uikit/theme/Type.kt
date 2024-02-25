7dad2359a457e5cce40227c3e410b86a0d053e72
ImNia <agnia-kimi1996@mail.ru>
2024-02-25 20:51:51 +0700

    Refactor typography

--- uikit/src/main/java/care/intouch/uikit/theme/Type.kt
+++ uikit/src/main/java/care/intouch/uikit/theme/Type.kt
@@ -15,7 +15,7 @@ val ralewayBold = FontFamily(Font(R.font.raleway_bold))
 
 //Regular
 val TitleAccentTypography = Typography(
-    displayLarge = TextStyle(
+    titleLarge = TextStyle(
         fontFamily = itimRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
@@ -24,28 +24,22 @@ val TitleAccentTypography = Typography(
     )
 )
 
-val TitleLargeTypography = Typography(
-    displayLarge = TextStyle(
+val TitleTypography = Typography(
+    titleLarge = TextStyle(
         fontFamily = itimRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 34.sp,
         letterSpacing = (-0.32).sp,
-    )
-)
-
-val TitleMediumTypography = Typography(
-    displayLarge = TextStyle(
+    ),
+    titleMedium = TextStyle(
         fontFamily = itimRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 21.sp,
         letterSpacing = (-0.32).sp,
-    )
-)
-
-val TitleSmallTypography = Typography(
-    displayLarge = TextStyle(
+    ),
+    titleSmall = TextStyle(
         fontFamily = itimRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
@@ -55,7 +49,7 @@ val TitleSmallTypography = Typography(
 )
 
 val SubTitleTypography = Typography(
-    displayLarge = TextStyle(
+    titleMedium = TextStyle(
         fontFamily = itimRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
@@ -65,7 +59,7 @@ val SubTitleTypography = Typography(
 )
 
 val BodyRegularTypography = Typography(
-    displayLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = ralewayRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 15.sp,
@@ -73,17 +67,14 @@ val BodyRegularTypography = Typography(
     )
 )
 
-val Caption1RegularTypography = Typography(
-    displayLarge = TextStyle(
+val CaptionRegularTypography = Typography(
+    bodyMedium = TextStyle(
         fontFamily = ralewayRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 13.sp,
         lineHeight = (15.5).sp,
-    )
-)
-
-val Caption2RegularTypography = Typography(
-    displayLarge = TextStyle(
+    ),
+    bodySmall = TextStyle(
         fontFamily = ralewayRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
@@ -92,7 +83,7 @@ val Caption2RegularTypography = Typography(
 )
 
 val FiltersRegularTypography = Typography(
-    displayLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = ralewayRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 15.sp,
@@ -101,7 +92,7 @@ val FiltersRegularTypography = Typography(
 )
 
 val TabBarTypography = Typography(
-    displayLarge = TextStyle(
+    labelSmall = TextStyle(
         fontFamily = ralewayRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 10.sp,
@@ -111,36 +102,33 @@ val TabBarTypography = Typography(
 
 //SemiBold
 val BodySemiBoldTypography = Typography(
-    displayLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = ralewaySemiBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 15.sp,
         lineHeight = 21.sp,
     )
 )
 
-val Caption1SemiBoldTypography = Typography(
-    displayLarge = TextStyle(
+val CaptionSemiBoldTypography = Typography(
+    bodyMedium = TextStyle(
         fontFamily = ralewaySemiBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 13.sp,
         lineHeight = (15.5).sp,
-    )
-)
-
-val Caption2SemiBoldTypography = Typography(
-    displayLarge = TextStyle(
+    ),
+    bodySmall = TextStyle(
         fontFamily = ralewaySemiBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
         lineHeight = (15.5).sp,
     )
 )
 
 val FiltersSemiBoldTypography = Typography(
-    displayLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = ralewaySemiBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 15.sp,
         lineHeight = 20.sp,
     )
@@ -148,36 +136,33 @@ val FiltersSemiBoldTypography = Typography(
 
 //Body
 val BodyBoldTypography = Typography(
-    displayLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = ralewayBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 15.sp,
         lineHeight = 21.sp,
     )
 )
 
-val Caption1BoldTypography = Typography(
-    displayLarge = TextStyle(
+val CaptionBoldTypography = Typography(
+    bodyMedium = TextStyle(
         fontFamily = ralewayBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 13.sp,
         lineHeight = (15.5).sp,
-    )
-)
-
-val Caption2BoldTypography = Typography(
-    displayLarge = TextStyle(
+    ),
+    bodySmall = TextStyle(
         fontFamily = ralewayBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 12.sp,
         lineHeight = (15.5).sp,
     )
 )
 
 val FiltersBoldTypography = Typography(
-    displayLarge = TextStyle(
+    bodyMedium = TextStyle(
         fontFamily = ralewayBold,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 15.sp,
         lineHeight = 20.sp,
     )
