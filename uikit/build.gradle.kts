9f57246e28391e74e7b0c9eba2473d2f18fed092
Ilnur Valiev <102486283+JudjinGM@users.noreply.github.com>
2024-03-15 13:11:58 +0400

    Arranged gradle dependencies

--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -39,16 +39,19 @@ android {
 }
 
 dependencies {
-
+    //Core
     implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-//    implementation(libs.material)
 
+    //Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+    //Testing
+    debugImplementation(libs.androidx.ui.tooling)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
