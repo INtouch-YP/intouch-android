8578c501c1066f842f2946d6f6311f4b3a10b0f7
ImNia <agnia-kimi1996@mail.ru>
2024-03-26 15:34:32 +0700

    Added shadow

--- uikit/src/main/java/care/intouch/uikit/ui/toggle/Toggle.kt
+++ uikit/src/main/java/care/intouch/uikit/ui/toggle/Toggle.kt
@@ -17,6 +17,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -48,6 +54,13 @@ fun Toggle(
                 .padding(2.dp)
                 .width(handleWidth)
                 .height(handleHeight)
+                .drawShadow(
+                    alpha = 0.15f,
+                    offsetX = 0.dp,
+                    offsetY = 3.dp,
+                    radius = handleHeight.value,
+                    blurRadius = 8.dp
+                )
                 .clip(CircleShape)
                 .background(
                     if (isChecked) InTouchTheme.colors.mainBlue
@@ -60,6 +73,34 @@ fun Toggle(
         )
     }
 }
+
+fun Modifier.drawShadow(
+    color: Color = Color.Black,
+    alpha: Float = 0.06f,
+    radius: Float = 0f,
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    blurRadius: Dp = 1.dp,
+) = this.drawBehind {
+    this.drawIntoCanvas {
+        val paint = Paint()
+        paint.asFrameworkPaint().apply {
+            this.color = color.toArgb()
+            this.setShadowLayer(
+                blurRadius.toPx(),
+                offsetX.toPx(),
+                offsetY.toPx(),
+                color.copy(alpha = alpha).toArgb()
+            )
+        }
+        it.drawCircle(
+            Offset((this.size.width + offsetX.value) / 2f, (this.size.height + offsetY.value) / 2f),
+            radius,
+            paint
+        )
+    }
+}
+
 
 @Preview
 @Composable
