7476b7b68b1d9842f2adeb8721d528b6fd0a48ae
ainur <exen1993@gmail.com>
2024-05-05 12:53:14 +0300

    added coroutine

--- app/src/main/java/care/intouch/app/feature/authorization/pinCode/presentation/PinCodeViewModel.kt
+++ app/src/main/java/care/intouch/app/feature/authorization/pinCode/presentation/PinCodeViewModel.kt
@@ -2,11 +2,14 @@ package care.intouch.app.feature.authorization.pinCode.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import care.intouch.app.feature.authorization.pinCode.data.Result
 import care.intouch.app.feature.authorization.pinCode.domain.InstallPinCodeUseCase
 import care.intouch.app.feature.authorization.pinCode.domain.ResetPinCodeUseCase
 import care.intouch.app.feature.authorization.pinCode.domain.VerifyPinCodeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,35 +20,38 @@ class PinCodeViewModel @Inject constructor(
 ): ViewModel() {
 
     fun init(){
-        installPinCodeUseCase.invoke("1234")
+        viewModelScope.launch(Dispatchers.IO) {
+            installPinCodeUseCase.invoke("1234")
 
-        when(val w = verifyPinCodeUseCase.invoke("1234")){
-            is Result.Success -> {
-                Log.d("TAG","Правильный " +  w.data.toString())
+            when(val w = verifyPinCodeUseCase.invoke("1234")){
+                is Result.Success -> {
+                    Log.d("TAG","Правильный " +  w.data.toString())
+                }
+                is Result.Error -> {
+                    Log.d("TAG","Правильный " + w.exception.toString())
+                }
             }
-            is Result.Error -> {
-                Log.d("TAG","Правильный " + w.exception.toString())
+
+            when(val w = verifyPinCodeUseCase.invoke("1214")){
+                is Result.Success -> {
+                    Log.d("TAG","НеПравильный " +  w.data.toString())
+                }
+                is Result.Error -> {
+                    Log.d("TAG","НеПравильный " + w.exception.toString())
+                }
+            }
+
+            installPinCodeUseCase.invoke("1134")
+
+            when(val w = verifyPinCodeUseCase.invoke("1234")){
+                is Result.Success -> {
+                    Log.d("TAG","Правильный второй " +  w.data.toString())
+                }
+                is Result.Error -> {
+                    Log.d("TAG","Правильный второй " + w.exception.toString())
+                }
             }
         }
 
-        when(val w = verifyPinCodeUseCase.invoke("1214")){
-            is Result.Success -> {
-                Log.d("TAG","НеПравильный " +  w.data.toString())
-            }
-            is Result.Error -> {
-                Log.d("TAG","НеПравильный " + w.exception.toString())
-            }
-        }
-
-        installPinCodeUseCase.invoke("1134")
-
-        when(val w = verifyPinCodeUseCase.invoke("1234")){
-            is Result.Success -> {
-                Log.d("TAG","Правильный второй " +  w.data.toString())
-            }
-            is Result.Error -> {
-                Log.d("TAG","Правильный второй " + w.exception.toString())
-            }
-        }
     }
 }
