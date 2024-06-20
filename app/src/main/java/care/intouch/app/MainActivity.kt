beee71d91d64a6baf1da460a3ba82861aec96e08
Vladimir Bolshakov <vladalexeco86@gmail.com>
2024-06-20 23:56:59 +0300

    FIX: Check App Build

--- app/src/main/java/care/intouch/app/MainActivity.kt
+++ app/src/main/java/care/intouch/app/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        Timber.tag("CheckAppBuild").d("Check App Build")
+        Timber.tag("CheckAppBuild").d("Check App Build 001")
     }
 
     override fun onResume() {
