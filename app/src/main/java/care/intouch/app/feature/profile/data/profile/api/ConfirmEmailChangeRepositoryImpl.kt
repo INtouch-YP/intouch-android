c4e4b09fc9bb1a1e093ef49632e0af735e27feae
Babaylov Anton <babaylov-ayu@mail.ru>
2024-07-17 21:53:39 +0500

    In progress

--- app/src/main/java/care/intouch/app/feature/profile/data/profile/api/ConfirmEmailChangeRepositoryImpl.kt
+++ app/src/main/java/care/intouch/app/feature/profile/data/profile/api/ConfirmEmailChangeRepositoryImpl.kt
@@ -2,12 +2,13 @@ package care.intouch.app.feature.profile.data.profile.api
 
 import care.intouch.app.feature.profile.domain.profile.useCase.ConfirmEmailChangeRepository
 import kotlinx.serialization.json.Json
+import javax.inject.Inject
 
-class ConfirmEmailChangeRepositoryImpl(
+class ConfirmEmailChangeRepositoryImpl @Inject constructor(
     private val confirmEmailChangeApi: ConfirmEmailChangeApi,
     private val json: Json
 ): ConfirmEmailChangeRepository {
     override suspend fun confirmEmailChange(id: String, token: String) {
-        TODO("Not yet implemented")
+
     }
 }
