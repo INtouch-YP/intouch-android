cf57ea9fc2eff9c0ebbb02fb2ad51b3aef84bc41
Babaylov7 <babaylov-ayu@mail.ru>
2024-07-17 15:16:26 +0500

    in progress

--- app/src/main/java/care/intouch/app/core/utils/mappers/DeepLinkResultWrapper.kt
+++ app/src/main/java/care/intouch/app/core/utils/mappers/DeepLinkResultWrapper.kt
@@ -2,5 +2,6 @@ package care.intouch.app.core.utils.mappers
 
 sealed class DeepLinkResultWrapper {
     data object ResetPasswordDeepLink: DeepLinkResultWrapper()
+    //data object EmailUpdateDeepLink: DeepLinkResultWrapper()
     data object AbsentDeepLink: DeepLinkResultWrapper()
 }
